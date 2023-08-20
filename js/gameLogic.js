db91a0c714c23775b49c592c3dcbbeea5cf1bf36
Mahmoud Elsayed <Mahmood.M.Elsaayed@gmail.com>
2023-08-20 12:36:23 +0300

    [feat] Add settings panel to homepage

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -1,13 +1,74 @@
-// SKETCH BOARD
-// Create the sketch-board's grid of cells
+// SKETCH BOARD SETUP
+// Create a grid of cells on the sketch board
 function createGrid(cellCount) {
     const sketchBoard = document.querySelector(".sketch-board");
-    let cellDimension = Math.sqrt(sketchBoard.clientHeight ** 2 / cellCount);
+    const cellDimension = Math.sqrt(sketchBoard.clientHeight ** 2 / cellCount);
     for (let i = 0; i < cellCount; i++) {
         const cell = document.createElement("div");
         cell.classList.add("cell");
-        cell.setAttribute("style", `background-color: white; width: ${cellDimension}px; height: ${cellDimension}px;`);
+        cell.style.cssText = `background-color: white; width: ${cellDimension}px; height: ${cellDimension}px;`;
         sketchBoard.appendChild(cell);
     }
 }
-createGrid(16*16) // create the default grid
+createGrid(16 * 16); // Create the default grid
+
+// MODE SELECTION BUTTONS
+const modesButtons = document.querySelectorAll(".btn");
+modesButtons.forEach(button => {
+    button.addEventListener("click", pickMode);
+});
+
+// Clear previous event listeners from cells
+function removeCellEventListeners() {
+    const gridCells = document.querySelectorAll(".cell");
+    gridCells.forEach(cell => {
+        cell.removeEventListener("mouseover", colorMode);
+        cell.removeEventListener("mouseover", rainbowMode);
+        cell.removeEventListener("mouseover", eraserMode);
+    });
+}
+
+// Select and apply drawing modes
+function pickMode(e) {
+    removeCellEventListeners(); // Remove previous event listeners
+    const gridCells = document.querySelectorAll(".cell");
+    gridCells.forEach(cell => {
+        if (e.target.id === "colorMode") {
+            cell.addEventListener("mouseover", colorMode);
+        } else if (e.target.id === "rainbowMode") {
+            cell.addEventListener("mouseover", rainbowMode);
+        } else if (e.target.id === "eraser") {
+            cell.addEventListener("mouseover", eraserMode);
+        } else {
+            cell.style.backgroundColor = "white";
+        }
+    });
+}
+
+// DRAWING MODES
+function colorMode(e) {
+    const colorInput = document.querySelector(".color-wheel");
+    e.currentTarget.style.backgroundColor = colorInput.value;
+}
+
+function rainbowMode(e) {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    e.currentTarget.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+}
+
+function eraserMode(e) {
+    e.currentTarget.style.backgroundColor = "white";
+}
+
+// GRID SIZE ADJUSTMENT
+const slider = document.querySelector("#slider");
+const sliderPara = document.querySelector(".slider-text");
+slider.addEventListener("input", () => {
+    const gridCells = document.querySelectorAll(".cell");
+    gridCells.forEach(cell => cell.remove());
+    const gridSize = slider.value * slider.value;
+    createGrid(gridSize);
+    sliderPara.textContent = `${slider.value} x ${slider.value}`;
+});
